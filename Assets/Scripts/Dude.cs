eeff94c45dc9f89c59fc3f593b20f6b69d9b77c4
fenru <saw106@case.edu>
2014-04-07 23:14:38 -0400

    Scripting
    Put some fields and methods in, created a script for enemy units.  Will
    expand on this base in class

---
+++ Assets/Scripts/Dude.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dude : MonoBehaviour {
+
+	public int health;
+	public int moneydropped;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void takeDamage(int damage) {
+		health -= damage;
+		if (health <= 0) {
+			//deal with money
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
+	public GameObject target;
+	public int Power;
+	public GameObject turret;
+	public double attackspeed;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	public void shoot() {
+		//fire at target
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (target != null) {
+			Vector3 amttorotate;
+			amttorotate = Vector3.RotateTowards (turret.transform.forward, target.transform.position - turret.transform.position, 6f, 6f);	
+			Turret.transform.rotation = Quaternion.LookRotation (amttorotate, new Vector3 (0f, 1f, 0f));
+		}
+
 	}
 }
