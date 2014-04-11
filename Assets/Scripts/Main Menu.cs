1d0715ff1044f7449285b888401e555fd1046ca4
Hun Jae Lee <hunjaelee@icloud.com>
2014-04-11 18:48:17 -0400

    Working on main menu
    Also got the barrels rotate for basic++ tower (imagine some sort of a
    vulcan cannon). for now it will rotate anytime but should rotate when
    it is firing.

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	private RaycastHit hitInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	//Based on the click a different interaction will happen on the main menu
+	void Update () {
+		
+		
+		if( Physics.Raycast( Camera.main.ScreenPointToRay( Input.mousePosition ), out hitInfo ) ){
+			if(Input.GetMouseButtonDown(0)){
+				//loads the Game Scen
+				if(hitInfo.collider.name == "Play Game"){
+					Application.LoadLevel(1);
+				}
+				//shoots the tank on the mainmenu
+			} 
+			
+		}
+	}
+	
+}
---
+++ Assets/Turrets/1_BasicTurret/RotateBarrel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* This script is for Basic3 tower (the one with rotating barrels)
+ * When firing, enable this script
+ * Attach to the "turret" prefab.
+ */
+ 
+public class RotateBarrel : MonoBehaviour {
+	public GameObject barrels;
+	Vector3 rotationAmount = new Vector3(0,0,1200f);
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		barrels.transform.Rotate(rotationAmount*Time.smoothDeltaTime);
+	}
+}
