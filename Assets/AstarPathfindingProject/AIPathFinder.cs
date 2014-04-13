7f2220bdc288a2ba482eb0c4281da55301bbbbb6
Kei Hitomi <khitomi337@gmail.com>
2014-04-13 15:33:10 -0400

    added a map and path finding system
    a map using blender was added and the units move on the map and go in
    different paths

---
+++ Assets/AstarPathfindingProject/AIPathFinder.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class AIPathFinder : MonoBehaviour {
+	public Transform target;
+	Seeker seeker;
+	Path path;
+	int currentWaypoint;
+	public float speed = 20;
+	public float wayPointDist = 2f;
+	CharacterController charctlr;
+
+	// Use this for initialization
+	void Start () {
+		seeker = GetComponent<Seeker>();
+		seeker.StartPath(transform.position,target.transform.position,OnPathComplete);
+		charctlr = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPathComplete(Path p){
+		if(!p.error){
+			path = p;
+			currentWaypoint = 0	;
+		}
+		else{
+
+		}
+
+	}
+	void FixedUpdate(){
+		if(path == null){
+			return;
+		}
+		if(currentWaypoint >=path.vectorPath.Count){
+			return;
+		}
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized * speed;
+		charctlr.SimpleMove(dir);
+		if(Vector3.Distance(transform.position,path.vectorPath[currentWaypoint]) < wayPointDist){
+			currentWaypoint++;
+		}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,20 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
-	public GameObject TilePrefab;
 	public GameObject TurretPrefab;
-	List <List<Tile>> map = new List<List<Tile>>();
 	public static Turret[] TurretList;
-	public int mapSize;
 	public bool turretIsSelected;
 	public static int layermask = 1<<9;
 	public static GameObject selectedTurret;
 	public static Turret selectedScript;
 	
 	// Use this for initialization
-	void Start () {
-		generateMap();
-		//generateTurrets();
+	void Start () {		//generateTurrets();
 	}
 	
 	// Update is called once per frame
@@ -33,19 +28,6 @@ public class GameManager : MonoBehaviour {
 		TurretList.Add(turret);
 		ally.Add(turret);
 	}*/
-	
-	void generateMap(){
-		map = new List<List<Tile>>();
-		for(int i = 0; i < mapSize; i++){
-			List <Tile> row = new List<Tile>();
-			for(int j = 0; j < mapSize; j++){
-				Tile tile = ((GameObject)Instantiate(TilePrefab, new Vector3(1.5f*i - Mathf.Floor(mapSize/2),0,-1.5f*j+Mathf.Floor(mapSize/2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
-				tile.gridPosition = new Vector2(i,j);
-				row.Add(tile);
-			}
-			map.Add(row);
-		}
-	}
 
 	void deselectTurret(){
 		if(turretIsSelected && Input.GetKey(KeyCode.Escape)){
