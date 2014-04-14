34d9542a387f90d8612b633d44db191404438cd7
Hun Jae Lee <hunjaelee@icloud.com>
2014-04-14 18:06:45 -0400

    Added Health Bar
    Need some position adjusting once we are done adding enemies

---
+++ Assets/Scripts/EnemyScript/FloatingHealthBar.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingHealthBar : MonoBehaviour {
+	public GameObject greenBar;
+	public GameObject redBar;
+	
+	public float currentHealth;
+	private float maxHealth;
+	private float temp;
+
+	// Sets the position of health bar with some offset
+	void Start () {
+		this.transform.localPosition = new Vector3(-1,5,0);
+		currentHealth = 100;
+		maxHealth = 100;
+	}
+
+	void Update () {
+
+	}
+
+
+	// @param adj the amount of health you want to adjust
+	// will keep adjusting the health bar's position
+	// for example, if you want the object to take 15 damage, you would have to use -15 instead of 15
+	public void health(int adj) {
+		currentHealth += adj;
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			Debug.Log ("Health zero. Dead!");
+			greenBar.transform.localPosition = new Vector3 (0.5f, 0, 0);
+			greenBar.transform.localScale = new Vector3 (0, greenBar.transform.localScale.y, greenBar.transform.localScale.z);
+
+		} else if (currentHealth >= maxHealth) {
+			// just in case we get current health exceeding max health
+			// readjusts the current health to max health (e.g. you can't have 120 out of 100 health)
+			// can change if we want something like mega health from quake, but we don't need/want that for now...
+			currentHealth = maxHealth;
+			Debug.Log ("health is over 100. readjusting it back to 100...");
+			greenBar.transform.localScale = new Vector3 (1,
+			                                             greenBar.transform.localScale.y,
+			                                             greenBar.transform.localScale.z);
+			
+			greenBar.transform.localPosition = new Vector3 (0,
+			                                                greenBar.transform.localPosition.y,
+			                                                greenBar.transform.localPosition.z);
+		} else {
+			greenBar.transform.Translate (-(adj / (maxHealth*2)), 0, 0);
+			greenBar.transform.localScale = new Vector3 ((currentHealth) / maxHealth,
+			                                             greenBar.transform.localScale.y,
+			                                             greenBar.transform.localScale.z);
+		}
+	}
+	
+}
