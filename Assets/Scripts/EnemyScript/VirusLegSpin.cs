a81ea539c733449d89e3c454c3e68222b8843a13
Hun Jae Lee <hunjaelee@icloud.com>
2014-04-15 02:21:33 -0400

    Created enemy
    looks derpy as hell…

---
+++ Assets/Scripts/EnemyScript/VirusLegSpin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirusLegSpin : MonoBehaviour {
+	public GameObject legs;
+	Vector3 rotationAmount = new Vector3(0,0,180f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		legs.transform.Rotate (rotationAmount * Time.smoothDeltaTime);
+	}
+}
