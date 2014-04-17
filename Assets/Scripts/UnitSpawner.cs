130337181489fa865b5814f2fc768bf929880d1c
Kei Hitomi <khitomi337@gmail.com>
2014-04-17 17:19:41 -0400

    Spawner added
    the units now spawn in the beginning.  When spawned the spawner gives
    the enemies the target and then the enemies move when their paths are
    determined

---
+++ Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitSpawner : MonoBehaviour {
+	public GameObject[] enemyUnits;
+	public static int waveCompleted;
+	public Transform target;
+	private int numUnits;
+	private float spawnRate;
+	public bool spawnReady;
+	private float spawnTime;
+	private int unit1Range;
+
+	// Use this for initialization
+	void Start () {
+		numUnits = 10;
+		unit1Range = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(numUnits == 0){
+			waveCompleted += 1;
+			spawnReady = false;
+			//this will need some testing
+			numUnits += (int)waveCompleted*5;
+			updateRandom();
+		}
+		if(spawnReady){
+			spawnUnit();
+		}
+	}
+	void spawnUnit(){
+		int select;
+		select = Random.Range(0,100);
+		if(select < unit1Range){
+			select = 0;
+		}
+		else{
+			select = 1;
+		}
+		GameObject unit = (GameObject)Instantiate(enemyUnits[select],transform.position,transform.rotation);
+		unit.GetComponent<AIPathFinder>().target = target;
+		numUnits -= 1;
+	}
+	//updates the ranges for the different types of units
+	void updateRandom(){
+		if(unit1Range <=0 ){
+			unit1Range = 1;
+		}
+		else{
+			unit1Range -= 5;
+		}
+	}
+}
