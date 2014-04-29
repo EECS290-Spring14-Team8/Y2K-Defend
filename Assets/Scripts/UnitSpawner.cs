7a485fb654179b13e85b0eef38dabeac6c0dee94
fenru <saw106@case.edu>
2014-04-29 13:39:47 -0400

    Unit and worm now separate like how nature intended
    had to change the scene to make sure the spawner had the right prefabs

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -10,12 +10,15 @@ public class UnitSpawner : MonoBehaviour {
 	public static bool spawnReady;
 	private float spawnTime;
 	private int unit1Range;
+	private int unit2Range;
 	public static int spawned = 0;
 
 	// Use this for initialization
 	void Start () {
 		numUnits = 10;
-		unit1Range = 50;
+		unit1Range = 33;
+		unit2Range = 66;
+
 	}
 	
 	// Update is called once per frame
@@ -38,12 +41,12 @@ public class UnitSpawner : MonoBehaviour {
 		Debug.Log(spawned);
 		int select;
 		select = Random.Range(0,100);
-		if(select < unit1Range){
-			select = 0;
-		}
-		else{
-			select = 1;
-		}
+		if (select < unit1Range) {
+						select = 0;
+				} else if (select < unit2Range) {
+						select = 1;
+				} else
+						select = 2;
 		GameObject unit = (GameObject)Instantiate(enemyUnits[select],transform.position,transform.rotation);
 		unit.GetComponent<AIPathFinder>().target = target;
 		numUnits -= 1;
