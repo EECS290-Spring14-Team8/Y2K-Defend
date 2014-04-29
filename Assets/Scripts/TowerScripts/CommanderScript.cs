7eb011e81425385c04461019c9088fb7f7c8c8a2
fenru <saw106@case.edu>
2014-04-29 09:56:44 -0400

    Commander Tower parts, not functional
    commander tower parts are in, with a placeholder model, still working on
    getting the thing to see towers nearby.

---
+++ Assets/Scripts/TowerScripts/CommanderScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CommanderScript : Turret {
+
+	public List<Turret> dudes = new List<Turret>();
+
+	// Use this for initialization
+	void Start () {
+		this.Power = 10;
+		this.attackspeed = .2f;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TowerScripts/CommanderTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommanderTrigger : MonoBehaviour {
+
+	public CommanderScript tower;
+	public int number = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<SphereCollider> ().radius = tower.range;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("tower")) {
+			other.gameObject.GetComponent<Turret>().attackspeed -= tower.attackspeed;
+			other.gameObject.GetComponent<Turret>().Power += tower.Power;
+			number++;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag ("tower")) {
+			other.gameObject.GetComponent<Turret>().attackspeed += tower.attackspeed;
+			other.gameObject.GetComponent<Turret>().Power -= tower.Power;
+			number--;
+		}
+	}
+}
